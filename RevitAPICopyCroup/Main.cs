aa2093a5b485053f5e79c0a960102d9fe7e23b16
Сидоров Артем <sidoroffartemii@gmail.com>
2025-03-10 16:53:31 +0300

    Добавьте файлы проекта.

---
+++ RevitAPICopyCroup/Main.cs
@@ -0,0 +1,45 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPICopyCroup
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            Reference reference=uidoc.Selection.PickObject(Autodesk.Revit.UI.Selection.ObjectType.Element, "Выберете группу объектов");
+
+            Element element = doc.GetElement(reference);
+            Group group = element as Group;
+            XYZ point = uidoc.Selection.PickPoint("Выберете точку");
+
+
+
+
+            using (var ts = new Transaction(doc, "Cope Group"))
+            {
+                ts.Start();
+
+                doc.Create.PlaceGroup(point, group.GroupType);
+
+                ts.Commit();
+
+            }
+
+
+            return Result.Succeeded;
+        }
+    }
+}
